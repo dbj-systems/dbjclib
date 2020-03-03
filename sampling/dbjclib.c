70adc8db6a1fd1afbc8a7eec2415f9d1be787212
dbjdbj <dbj@dbj.org>
2020-03-03 11:46:10 +0100

    rant in a comment about munit

--- sampling/dbjclib.c
+++ sampling/dbjclib.c
@@ -9,13 +9,23 @@
 #pragma warning(disable : 4127)
 #endif
 
-/* DBJ: This is a lot of code for 5 unit tests */
+/*
+ DBJ: This is a lot of code for 5 unit tests 
+ test unit functions are in separate C files
+ they are collected here and the whole MUNIT API 
+ shenanigans are done so we can have them
+ separated by our logic when displaying the results
+
+ The outcome is a single console executable with non 
+ trivial set of CLI arguments that can be used
+ or has to be used; it depends on the user requirements
+ */
 /**********************************************************************/
-extern MunitResult dbj_string_trim_test(const MunitParameter params[], void *data);
-extern MunitResult dbj_string_list_performance(const MunitParameter params[], void *data);
-extern MunitResult dbj_string_list_precision(const MunitParameter params[], void *data);
-extern MunitResult dbj_sll_test(const MunitParameter params[], void *data);
-extern MunitResult dbj_fb_string_test(const MunitParameter params[], void *data);
+extern MunitResult dbj_string_trim_test(const MunitParameter [], void *);
+extern MunitResult dbj_string_list_performance(const MunitParameter [], void *);
+extern MunitResult dbj_string_list_precision(const MunitParameter [], void *);
+extern MunitResult dbj_sll_test(const MunitParameter [], void *);
+extern MunitResult dbj_fb_string_test(const MunitParameter [], void *);
 
 /**********************************************************************/
 #define DBJ_MUNIT_TEST_ADD( TITLE_, FP_) {(char *)TITLE_, FP_, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
@@ -24,7 +34,9 @@ extern MunitResult dbj_fb_string_test(const MunitParameter params[], void *data)
 #define DBJ_MUNIT_SUITE_EOL { NULL, NULL, 0, 0, MUNIT_SUITE_OPTION_NONE }
 #define DBJ_MUNIT_SUITE_ADD( TITLE_, AP_) {(char *)TITLE_, AP_, NULL, 1, MUNIT_SUITE_OPTION_NONE} 
 /**********************************************************************/
-/* first need to create arrays of tets, or just one but that is not practical */
+/* 
+first need to create arrays of tests, or just one but that is not practical 
+*/
 static MunitTest string_trim_tests[] = {
     DBJ_MUNIT_TEST_ADD("/basic test", dbj_string_trim_test),
     DBJ_MUNIT_TEST_EOL };
@@ -42,6 +54,9 @@ static MunitTest sll_tests[] = {
     DBJ_MUNIT_TEST_ADD("/basic", dbj_sll_test),
     DBJ_MUNIT_TEST_EOL};
 
+/* 
+now need to create array of suites
+*/
 static MunitSuite suites_array[] = { 
   DBJ_MUNIT_SUITE_ADD("/string trim tests", string_trim_tests) ,
   DBJ_MUNIT_SUITE_ADD("/string list tests", string_list_tests),
@@ -50,6 +65,9 @@ static MunitSuite suites_array[] = {
   DBJ_MUNIT_SUITE_EOL
   };
 
+/* 
+now need to create top level suite
+*/
 static MunitSuite top_collection =
     {(char *)"dbjCLIB", NULL, suites_array , 1, MUNIT_SUITE_OPTION_NONE};
 
@@ -64,7 +82,7 @@ int main(int argc, char *argv[MUNIT_ARRAY_PARAM(argc + 1)])
   system("@chcp 65001 >NUL");
 #endif
   /* That second argument
-   * is the user_data parameter which will be passed either to the
+   * is the user_ parameter which will be passed either to the
    * test or (if provided) the fixture setup function. */
   return munit_suite_main(&top_collection, (void *)NULL, argc, argv);
 }
